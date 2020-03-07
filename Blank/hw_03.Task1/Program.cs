87b4a1bb507eff8fa218e50f285e5ffd26a3b8cc
IvanTrepashko <goodfellawk@gmail.com>
2020-03-07 21:46:23 +0300

    Almost done

---
+++ Blank/hw_03.Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace hw_03.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 10;
+            int b = 40;
+            long c = 10;
+            float d = 15.34f;
+            double z;
+            char ch;
+            bool f = true;
+
+            // Implicit conversion
+
+            b = a;
+            c = b;
+            z = d;
+            
+            //Explicit conversion
+
+            a = (short)b;
+            b = (int)d;
+            ch = (char)b;
+
+            //Boxing
+       
+            Object ob_int, ob_bool, ob_char;
+            ob_int = b;
+            ob_bool = f;
+            ob_char = ch;
+
+            // Unboxing
+
+            ushort sh =(ushort)(int)ob_int;
+            int it = (int)(char)ob_char;
+
+        }   
+    }
+}
