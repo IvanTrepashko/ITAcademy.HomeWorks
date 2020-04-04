18160ad8366d3ab48f45c431b3acf0e8eb9d5594
IvanTrepashko <goodfellawk@gmail.com>
2020-04-04 12:21:15 +0300

    Task 1 done

---
+++ Blank/hw_08/hw_08.Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace hw_08.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a poem in one string");
+            string initialPoem;
+            initialPoem = Console.ReadLine();
+            string[] splittedPoem = initialPoem.Split(';');
+            for (int i = 0; i < splittedPoem.Length; i++)
+            {
+                splittedPoem[i] = splittedPoem[i].Replace('O', 'A');
+                splittedPoem[i] = splittedPoem[i].Trim();
+            }
+            foreach (string item in splittedPoem)
+                Console.WriteLine(item);
+        }
+    }
+}
