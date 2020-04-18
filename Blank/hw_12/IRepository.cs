e26a135c68581a0c398c2c7ae78b1cb8cd2a6772
IvanTrepashko <goodfellawk@gmail.com>
2020-04-18 21:31:06 +0300

    Homework done

---
+++ Blank/hw_12/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace hw_12
+{
+    public interface IRepository<T>
+    {
+        void Create(T obj);
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        void Update(int id);
+        void Delete(int id);
+    }
+}
